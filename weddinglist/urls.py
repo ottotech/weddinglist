269f95255a52c22837179156fe5a455f99902700
Otto Schuldt <ottoschuldt@gmail.com>
2020-09-29 18:40:16 +0200

    adds authentication and templates

--- weddinglist/urls.py
+++ weddinglist/urls.py
@@ -1,21 +1,17 @@
-"""weddinglist URL Configuration
+# -*- coding: utf-8 -*-
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# Django imports
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+
+# Project imports
+from weddinglist.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login', LoginView.as_view(), name='login'),
+    path('logout', LogoutView.as_view(), name='logout'),
+    path('', home, name='home'),
 ]
---
+++ weddinglist/views.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from django.shortcuts import render, redirect
+from django.conf import settings
+
+
+def home(request):
+    if not request.user.is_authenticated:
+        return redirect(settings.LOGIN_URL)
+    return render(request, 'home.html', {'home': True})
